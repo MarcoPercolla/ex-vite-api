6d10cab109ab1b648ec593dd7cc487cd55cce833
ReDMallard <marco@titori.it>
2023-11-30 15:32:58 +0100

    predisposizione iniziale (ps. HelloWorld template)

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from 'axios'; //importo Axios
+
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+    this.getBeer();
+  },
+  methods: {
+    getBeer() {
+      axios.get(`https://api.openbrewerydb.org/v1/breweries?by_country=scotland&per_page=10`).then(risultato => {
+        console.log(risultato.data)
+      });
+    },
+  }
+}
+</script>
+<template>
+  <div>prova</div>
+</template>
+<style scoped></style>
